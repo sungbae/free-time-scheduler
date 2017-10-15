02dd7590a8a890eab33c1781062af2f610657fa1
Sung Bae <sungbae@Sungs-MBP.hsd1.co.comcast.net>
2017-10-14 23:57:11 -0600

    added ORMLite and SQLite

--- FreeTime/app/src/main/java/com/example/android/freetime/MainActivity.java
+++ FreeTime/app/src/main/java/com/example/android/freetime/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.freetime;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.templabel);
+        MyDatabase db = new MyDatabase(this);
+        Cursor user = db.getUsers();
+        String firstName = user.getString(0);
+        String lastName = user.getString(1);
+        tv.setText("Hello " + firstName + " " + lastName + "!");
     }
 }
---
+++ FreeTime/app/src/main/java/com/example/android/freetime/MyDatabase.java
@@ -0,0 +1,37 @@
+package com.example.android.freetime;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+/**
+ * Created by sungbae on 10/14/17.
+ */
+
+public class MyDatabase extends SQLiteAssetHelper {
+
+    private static final String DATABASE_NAME = "FreeTime.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public MyDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public Cursor getUsers() {
+        SQLiteDatabase db = getReadableDatabase();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+        String [] sqlSelect = {"FirstName", "LastName"};
+        String sqlTables = "User";
+
+        qb.setTables(sqlTables);
+        Cursor c = qb.query(db, sqlSelect, null, null,
+                null, null, null);
+
+        c.moveToFirst();
+        return c;
+    }
+}
